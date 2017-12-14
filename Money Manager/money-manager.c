653b16722139e0cb648292a1010f3038ec88bfae
Tifan Dwi Avianto <ifan.anvity@gmail.com>
2017-12-14 19:48:21 +0700

    Add 2 new programs

---
+++ Money Manager/money-manager.c
@@ -0,0 +1,228 @@
+/*
+==============================================================================
+Author	: Tifan Dwi Avianto (github.com/ifananvity)
+NIM		: A11.2017.10629 / A11.4113
+Version	: 1.0 - 11/12/2017
+Desc	: Money manager. Calculates income and expenses for x(user input) days
+==============================================================================
+*/
+
+// Libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+// Declaration
+char menuSelection;
+char trxSelection;
+int balance;
+int askDays, askTrx;
+int trx, totalTrx, total;
+int income[10], incomeSource[10];
+int expense[10];
+int flag, flag2;
+int k = 0;
+int l = 0;
+
+// Constants definitions
+#define TRUE 1
+#define FALSE 0
+
+// Function prototypes
+void printTitle(void);
+void promptInitialLaunch(void);
+void mainMenu(void);
+void transactionMenu(void);
+void incomeTransaction(void);
+void expenseTransaction(void);
+void printStats(void);
+
+int main(void) {
+	printTitle();
+	promptInitialLaunch();
+	mainMenu();
+	
+	return EXIT_SUCCESS;
+}
+
+/* ------------------------------ FUNCTIONS ------------------------------ */
+
+void printTitle() {
+	printf("               Money McMoneyface\n");
+	printf("     \"Budget, finance and expense tracker.\"\n\n");
+}
+
+void promptInitialLaunch() {
+	printf("Welcome! First off, add your current balance.\n");
+
+	// ask for current balance
+	printf("Current balance: ");
+	scanf("%d", &balance);
+}
+
+void mainMenu() {
+	flag = FALSE;
+	while (!flag) {
+		printf("\n-------- MENU --------\n");
+		printf("Balance: Rp.%d\n", balance);
+		printf("1. Transaction\n");
+		printf("2. Stats\n");
+		printf("0. Exit\n");
+		fflush(stdin);
+		menuSelection = getchar();
+
+		switch (menuSelection) {
+			case '1' : {
+				transactionMenu();
+				break;
+			}
+			case '2' : {
+				printStats();
+				break;
+			}
+			case '0' : {
+				flag = TRUE;
+				break;
+			}
+			default: {
+				printf("Invalid input\n");
+			}
+		}
+	}
+}
+
+void transactionMenu() {
+	flag2 = FALSE;
+	while (!flag2) {
+		printf("\n-------- Transaction --------\n");
+		printf("1. Income\n");
+		printf("2. Expense\n");
+		printf("0. < Menu\n");
+		fflush(stdin);
+		trxSelection = getchar();
+
+		switch (trxSelection) {
+			case '1' : {
+				incomeTransaction();
+				flag2 = TRUE;
+				break;
+			}
+			case '2' : {
+				expenseTransaction();
+				flag2 = TRUE;
+				break;
+			}
+			case '0' : {
+				flag2 = TRUE;
+				break;
+			}
+			default : {
+				printf("Invalid input\n");
+			}
+		}
+	}
+}
+
+void incomeTransaction() {
+	printf("\n-------- Income --------\n");
+	printf("1. Allowance\n");
+	printf("2. Salary\n");
+	printf("3. Petty cash\n");
+	printf("4. Bonus\n");
+	printf("5. Other\n");
+	scanf("%d", &incomeSource[k]);
+
+	printf("Amount: ");
+	scanf("%d", &income[k]);
+
+	// add income to balance
+	balance += income[k];
+
+	// increment
+	k++;
+}
+
+void expenseTransaction() {
+	printf("\n-------- Expense --------\n");
+	printf("How many days would you want to input your expenses? ");
+	scanf("%d", &askDays);
+
+	for (int i = 1; i <= askDays; i++) {
+		// set initial value
+		totalTrx = 0;
+		
+		printf("Day %d\n", i);
+		printf("  How many transaction? ");
+		scanf("%d", &askTrx);
+		for (int j = 1; j <= askTrx; j++) {
+			if (j == 1) {
+				printf("    1st transaction: ");
+			} else if (j == 2) {
+				printf("    2nd transaction: ");
+			} else if (j == 3) {
+				printf("    3rd transaction: ");
+			} else {
+				printf("    %dth transaction: ", j);
+			}
+			scanf("%d", &trx);
+
+			totalTrx += trx;
+		}
+		printf("  Expenses on day %d is Rp.%d\n", i, totalTrx);
+
+		expense[l] = totalTrx;
+		total += totalTrx;
+
+		// subtract balance from expenses
+		balance -= total;
+
+		// increment
+		l++;
+	}
+	printf("Total expenditure: Rp.%d\n", total);
+}
+
+void printStats() {
+	printf("\n-------- Stats --------\n");
+	if (income[0] == 0 && expense[0] == 0) {
+		printf("No data available.\n");
+	} else {
+		printf("Balance: Rp.%d\n", balance);
+
+		// income history
+		printf("-Incomes\n");
+		for (int o = 0; o < k; o++) {
+			printf(" %d ", income[o]);
+			switch (incomeSource[o]) {
+				case 1 : {
+					printf("(Allowance)\n");
+					break;
+				}
+				case 2 : {
+					printf("(Salary)\n");
+					break;
+				}
+				case 3 : {
+					printf("(Petty cash)\n");
+					break;
+				}
+				case 4 : {
+					printf("(Bonus)\n");
+					break;
+				}
+				case 5 : {
+					printf("(Other)\n");
+					break;
+				}
+				default : {
+					printf("(Null)\n");
+				}
+			}
+		}
+
+		// expenses history
+		printf("-Expenses\n");
+		for (int p = 0; p < l; p++) {
+			printf(" %d\n", expense[p]);
+		}
+	}
+}
---
+++ Tollway/tollway.c
@@ -0,0 +1,162 @@
+/*
+==============================================================================
+Author	: Tifan Dwi Avianto (github.com/ifananvity)
+NIM		: A11.2017.10629 / A11.4113
+Version	: 1.0 - 12/12/2017
+Desc	: Assess fees on tollway depending on vehicle type and calculate total
+          income 
+==============================================================================
+*/
+
+// Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+// Declaration
+int car, bus, truck;
+char username[10];
+char password[256] = {0}, c;
+int pos;
+int flag;
+int counter;
+
+// Constants definitions
+#define CAR_FEE 2000
+#define BUS_FEE 3000
+#define TRUCK_FEE 5000
+#define TRUE 1;
+#define FALSE 0;
+
+// Function prototypes
+void inputPassword(void);
+void mainMenu(void);
+int inputAmount(void);
+void summary(void);
+void exitMainMenu(void);
+
+int main(void) {
+	// set initial value
+	counter = 2;
+
+	while (counter >= 0) {
+		// prompt for username and password
+		printf("Username= ");
+		scanf("%s", username);
+		printf("Password= ");
+		inputPassword();
+
+		// validate username and password
+		if (!strcmp(username, "admin") && !strcmp(password, "admin")) {
+			mainMenu();
+		} else {
+			printf("Wrong username or password. Try again. %d attempt left\n\n", counter);
+
+			// decrement counter
+			counter--;
+		}
+	}
+	
+	return EXIT_SUCCESS;
+}
+
+/* ------------------------------ FUNCTIONS ------------------------------ */
+
+void mainMenu() {
+	flag = FALSE;
+	while (!flag) {
+		printf("\n---------- MENU ----------\n");
+		printf("Add vehicles:\n");
+		printf("1. Car			4. Summary\n");
+		printf("2. Bus			0. Exit\n");
+		printf("3. Truck\n");
+		fflush (stdin);
+		char selection = getchar();
+
+		switch (selection) {
+			case '1' : {
+				car += inputAmount();
+				break;
+			}
+			case '2' : {
+				bus += inputAmount();
+				break;
+			}
+			case '3' : {
+				truck += inputAmount();
+				break;
+			}
+			case '4' : {
+				summary();
+				break;
+			}
+			case '0' : {
+				exitMainMenu();
+				break;
+			}
+			default : {
+				printf("Invalid input\n");
+			}
+		}
+
+		// reset counter
+		counter = 2;
+	}
+}
+
+void inputPassword() {
+	// Change password input to asterisk "*" (hides it)
+	pos = 0;
+	do {
+    	c = getch();
+
+    	if(isprint(c)) {
+        	password[pos++] = c;
+        	printf("%c", '*');
+    	}
+    	else if(c == '\b' && pos) {
+        	password[pos--] = '\0';
+        	printf("%s", "\b \b");
+    	}
+	} while(c != '\r');
+
+	printf("\n");
+}
+
+int inputAmount() {
+	int temp;
+
+	// ask the amount of vehicle has entered
+	printf("Amount: ");
+	scanf("%d", &temp);
+	return temp;
+}
+
+void summary() {
+	printf("\n-------- Summary --------\n");
+	if (car == 0 && bus == 0 && truck == 0) {
+		printf("No data available.\n");
+	} else {
+		printf("Car   : %d\n", car);
+		printf("Bus   : %d\n", bus);
+		printf("Truck : %d\n", truck);
+		printf("Total : Rp.%d,-\n", car * CAR_FEE + bus * BUS_FEE + truck * TRUCK_FEE);
+		
+	}
+	printf("Press any key to continue . . ."); getch();printf("\n");
+}
+
+void exitMainMenu() {
+	printf("Warning: Data will be lost. Continue? (y/n) ");
+	fflush(stdin);
+	char warning = getchar();
+	printf("\n");
+
+	if (warning == 'y') {
+		// reset
+		car = bus = truck = 0;
+
+		flag = TRUE;
+	}
+}
